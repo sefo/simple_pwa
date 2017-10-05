80232634a6f088baed0454505130e18ef55ead07
sefo <fournierse@gmail.com>
2017-10-05 20:32:26 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+(function() {
+    'use strict';
+
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('sw.js', {
+            scope: '/'
+        });
+    }
+
+    var bg = document.getElementById('bg');
+    var callme = document.getElementById('callme');
+    var arrow = document.getElementById('arrow');
+    var swipeMe = document.getElementById('swipeMe');
+
+    arrow.addEventListener('click', swipe);
+    callme.addEventListener('click', call);
+    
+    function swipe() {
+        bg.classList.toggle('blurred');
+        swipeMe.classList.toggle('close');
+        arrow.classList.toggle('up');
+    }
+
+    function call() {
+        swipe();
+        console.log("calling...");
+    }
+
+})();
---
+++ sw.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var CACHE_NAME = 'emergency-v1';
+var urlsToCache = [
+  '/',
+  '/assets/help.gif',
+  '/assets/beer.jpg',
+  '/js/main.js',
+  '/styles/main.css'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request));
+});
